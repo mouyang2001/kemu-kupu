e7cf97260f1dc9101e818ccb3a664b11a5a74aaa
bjur781 <bjur781@aucklanduni.ac.nz>
2021-09-18 14:00:07 +1200

    topic design

--- src/application/scene/SceneManager.java
+++ src/application/scene/SceneManager.java
@@ -27,16 +27,33 @@ public class SceneManager {
         // set and show stage
         stage.setScene(scene);
         stage.show();
+
+        
     }
 
     public void switchToTopicScene(ActionEvent event) throws IOException {
         // load fxml file
         root = FXMLLoader.load(getClass().getResource("./fxml/Topic.fxml"));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
+        scene = new Scene(root, stage.getWidth(), stage.getHeight());
 
         // load css file
         String homeSceneCss = this.getClass().getResource("./css/Topic.css").toExternalForm();
+        scene.getStylesheets().add(homeSceneCss);
+
+        // set and show stage
+        stage.setScene(scene);
+        stage.show();
+    }
+    
+    public void switchToQuizScene(ActionEvent event) throws IOException {
+        // load fxml file
+        root = FXMLLoader.load(getClass().getResource("./fxml/Quiz.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root, stage.getWidth(), stage.getHeight());
+
+        // load css file
+        String homeSceneCss = this.getClass().getResource("./css/Quiz.css").toExternalForm();
         scene.getStylesheets().add(homeSceneCss);
 
         // set and show stage
--- src/application/scene/Topic.java
+++ src/application/scene/Topic.java
@@ -1,6 +1,17 @@
 package application.scene;
 
-public class Topic {
+import java.io.IOException;
 
+import javafx.event.ActionEvent;
 
+public class Topic extends SceneManager{
+
+	
+	public void startQuiz(ActionEvent e) throws IOException {
+		switchToQuizScene(e);
+	}
+	
+	/*
+	 * TODO: functionality of choosing topic from list
+	 */
 }
