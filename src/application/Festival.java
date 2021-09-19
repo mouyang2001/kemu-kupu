1144733611910b9752cad5c71cc973d00f0aa622
Callum Iddon <iddoncallum@gmail.com>
2021-09-19 14:56:47 +1200

    Festival

--- src/application/Festival.java
+++ src/application/Festival.java
@@ -1,8 +1,6 @@
 package application;
 
 import java.io.IOException;
-import java.io.OutputStream;
-import java.io.PrintWriter;
 
 /**
  * Controls the festival text to speech command.
@@ -12,23 +10,10 @@ public class Festival {
 	 * Speaks text to the user.
 	 *
 	 * @param text Text to speak.
-	 * @param block If the operation should block until complete.
 	 * @throws IOException If an I/O error occurs.
 	 * @throws InterruptedException If the current thread is interrupted while blocking.
 	 */
-	public static void speak(String text, boolean block) throws IOException, InterruptedException {
-		// Start festival.
-		Process process = new ProcessBuilder("festival", "--tts").start();
-
-		// Send the text to standard input.
-		OutputStream in = process.getOutputStream();
-		PrintWriter stdin = new PrintWriter(in);
-		stdin.print(text);
-		stdin.close();
+	public static void speak(String text) throws IOException, InterruptedException {
 		
-		// Block until festival has stopped speaking.
-		if (block) {
-			process.waitFor();
-		}
 	}
 }
