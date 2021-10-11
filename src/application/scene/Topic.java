6080dd6c5a03b60db796386a82e5848d74bfcd0f
bjur781 <bjur781@aucklanduni.ac.nz>
2021-10-12 11:18:34 +1300

    added allwords list

--- src/application/scene/Topic.java
+++ src/application/scene/Topic.java
@@ -10,50 +10,52 @@ import javafx.scene.input.MouseEvent;
 
 /** Topic selection screen for the user to choose which word list to quiz. */
 public class Topic {
-  @FXML private Label title;
+	@FXML private Label title;
 
-  @FXML private ListView<String> topicListView;
+	@FXML private ListView<String> topicListView;
 
-  @FXML private Button back;
+	@FXML private Button back;
 
-  @FXML private Button start;
+	@FXML private Button start;
 
-  @FXML private Button practice;
+	@FXML private Button practice;
 
-  /** Called once the controller is loaded. */
-  @FXML
-  public void initialize() {
-    try {
-      // Add items to table view.
-      topicListView.getItems().addAll(Words.getTopics());
-    } catch (IOException e) {
-      SceneManager.alert("Could not load topics.");
-    }
+	/** Called once the controller is loaded. */
+	@FXML
+	public void initialize() {
+		try {
+			// Add items to table view.
+			topicListView.getItems().add(0, Words.getTopics().get(12));
+			topicListView.getItems().addAll(Words.getTopics());
 
-    // Enable start button once an item is selected.
-    topicListView.addEventHandler(
-        MouseEvent.MOUSE_PRESSED,
-        e -> {
-          start.setDisable(topicListView.getSelectionModel().getSelectedItem() == null);
-          practice.setDisable(topicListView.getSelectionModel().getSelectedItem() == null);
-        });
-  }
+		} catch (IOException e) {
+			SceneManager.alert("Could not load topics.");
+		}
 
-  /** Click handler for the start quiz button. */
-  @FXML
-  private void startQuiz() {
-    SceneManager.switchToQuizScene(topicListView.getSelectionModel().getSelectedItem());
-  }
+		// Enable start button once an item is selected.
+		topicListView.addEventHandler(
+				MouseEvent.MOUSE_PRESSED,
+				e -> {
+					start.setDisable(topicListView.getSelectionModel().getSelectedItem() == null);
+					practice.setDisable(topicListView.getSelectionModel().getSelectedItem() == null);
+				});
+	}
 
-  @FXML
-  private void startPractice() {
-    SceneManager.switchToPracticeScene(topicListView.getSelectionModel().getSelectedItem());
-  }
-  ;
+	/** Click handler for the start quiz button. */
+	@FXML
+	private void startQuiz() {
+		SceneManager.switchToQuizScene(topicListView.getSelectionModel().getSelectedItem());
+	}
 
-  /** Click handler for backing out of topic selection to main menu again. */
-  @FXML
-  private void leaveQuiz() {
-    SceneManager.switchToMenuScene();
-  }
+	@FXML
+	private void startPractice() {
+		SceneManager.switchToPracticeScene(topicListView.getSelectionModel().getSelectedItem());
+	}
+	;
+
+	/** Click handler for backing out of topic selection to main menu again. */
+	@FXML
+	private void leaveQuiz() {
+		SceneManager.switchToMenuScene();
+	}
 }
