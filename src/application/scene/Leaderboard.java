1546f301ef3f4fc32f03eba1c1697ffce57c81b8
bjur781 <bjur781@aucklanduni.ac.nz>
2021-10-14 13:41:07 +1300

    made leaderboard scene

--- src/application/scene/Leaderboard.java
+++ src/application/scene/Leaderboard.java
@@ -1,13 +1,22 @@
 package application.scene;
 
 import application.Statistics;
+import javafx.fxml.FXML;
 
 public class Leaderboard {
 	private int score;
+	private Statistics stats;
 
 	public void initialise(Statistics statistics) {
+		this.stats = stats;
 		score = statistics.getScore();
-		
+
 	}
+	
+	  /** Click handler to go back to finish screen. */
+	  @FXML
+	  private void back() {
+	    SceneManager.switchToFinishScene(stats, false);
+	  }
 
 }
